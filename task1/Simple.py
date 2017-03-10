f0802caea7215af59bbd9930b640def7fcd0b8d7
skittle456 <bagpigham@gmail.com>
2017-03-10 18:19:14 +0700

    ham
    test

--- task1/Simple.py
+++ task1/Simple.py
@@ -38,12 +38,40 @@ class Simple1(Simple):
 class Simple2(Simple):
 '''
 
-'''
-class Simple3(Simple):
-'''
+
+class Simple3(QWidget):
+    def __init__(self):
+        QWidget.__init__(self,None)
+        self.setWindowTitle("Ham Drawing")
+        self.rabbit = QImage("images/rabbit.png")
+        
+
+    def paintEvent(self,e):
+        p = QPainter()
+        p.begin(self)
+       
+        p.setPen(QColor(250,250,250))
+        p.setBrush(QColor(0,127,0))
+        p.drawPolygon([
+            QPoint(70,100), QPoint(100,110,),
+            QPoint(130,100), QPoint(100,150),
+            ])
+
+        p.setPen(QColor(255,127,0))
+        p.setBrush(QColor(255,127,0))
+        p.drawPie(50,150,100,100,0,180*16)
+
+        p.drawPolygon([
+            QPoint(50,200), QPoint(150,200),QPoint(100,400),
+            ])
+
+        p.drawImage(QRect(200,100,320,320),self.rabbit)
+        p.drawImage(QRect(520,100,320,320),self.rabbit)
+        p.end()
+
 
 app = QApplication(sys.argv)
-
+#s = Simple3()
 s = Simple2()
 s.show()
 app.exec_()
