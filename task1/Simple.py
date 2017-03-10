2db8ddf4fabe4221c313c3313acf379659e771af
iceyokuna <iceyokuna@hotmail.com>
2017-03-10 18:11:45 +0700

    Add files via upload

---
+++ task1/Simple.py
@@ -0,0 +1,49 @@
+import PySide
+import sys
+from PySide.QtGui import *
+from PySide.QtCore import *
+
+
+class Simple(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple2 Drawing")
+        self.rabbit = QImage("images/ rabbit.png")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0,0,0))
+        p.setBrush(QColor(0,127,0))
+
+        p.setPen(QColor(0,0,0))
+        p.setBrush(QColor(0,127,0))
+        p.drawPolygon([QPoint(70,100) , QPoint(100,110), QPoint(130,100),QPoint(100,150)])
+
+        p.setPen(QColor(255,127,0))
+        p.setBrush(QColor(255,127,0))
+        p.drawPie(50,150,100,100,0,180*16)
+
+        p.drawPolygon([QPoint(50,200),QPoint(150,200),QPoint(100,400)])
+
+        p.drawImage(QRect(200,100,320,320), self.rabbit)
+        p.end()
+
+'''
+class Simple1(Simple):
+'''
+
+'''
+class Simple2(Simple):
+'''
+
+'''
+class Simple3(Simple):
+'''
+
+app = QApplication(sys.argv)
+
+s = Simple2()
+s.show()
+app.exec_()
