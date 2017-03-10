ff2dd360a6a1334e8b1a2bad73b658aedc0d3098
skittle456 <bagpigham@gmail.com>
2017-03-10 19:44:21 +0700

    ham
    button

--- task1/Simple.py
+++ task1/Simple.py
@@ -116,4 +116,28 @@ s3.show()
 
 app.exec_()
 
+'''#button
+class Paint(QWidget):
+    def __init__(self):
+        QWidget.__init__(self,None)
+        
+        layout = QVBoxLayout()
+        layout.addStretch(1)
+        self.setWindowTitle('Drawing')
+        self.setMinimumSize(600,600)
+        draw_label = QLabel("\t\t\tDrag the mouse to draw")
+        layout.addWidget(draw_label)
+        
+        #layout.addStretch(1)
+        clear_button = QPushButton("Clear")
+        layout.addWidget(clear_button)
+        clear_button.clicked.connect(self.clear_screen)
+        clear_button.setGeometry(300,300,300,300)
+        #layout.addStretch(1)
+        self.setLayout(layout)
+
+    def clear_screen(self):
+        "Insert Code Here"
+        print("Clear")
+'''
 
