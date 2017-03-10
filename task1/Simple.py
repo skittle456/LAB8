8f36e13d23ec2522f0f2e6699f7aa35fa06ac99a
bas00445 <lnw00445@gmail.com>
2017-03-10 18:17:33 +0700

    simple2_edited

--- task1/Simple.py
+++ task1/Simple.py
@@ -34,9 +34,27 @@ class Simple(QWidget):
 class Simple1(Simple):
 '''
 
-'''
+
 class Simple2(Simple):
-'''
+    def __init__(self):
+        super().__init__()
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(100,100,100))
+        p.setBrush(QColor(0,0,255))
+        p.drawPolygon([QPoint(70,100) , QPoint(100,110), QPoint(130,100),QPoint(100,150)])
+
+        p.setPen(QColor(200,127,0))
+        p.setBrush(QColor(200,127,0))
+        p.drawPie(50,150,100,100,0,180*16)
+
+        p.drawPolygon([QPoint(50,200),QPoint(150,200),QPoint(100,400)])
+
+        p.drawImage(QRect(200,100,320,320), self.rabbit)
+        p.end()    
 
 '''
 class Simple3(Simple):
@@ -44,6 +62,6 @@ class Simple3(Simple):
 
 app = QApplication(sys.argv)
 
-s = Simple2()
+s = Simple()
 s.show()
 app.exec_()
